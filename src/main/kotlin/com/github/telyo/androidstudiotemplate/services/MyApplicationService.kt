a29f690e8bc48c77d7607a6b0c8d95faa617a31c
GitHub Action <action@github.com>
2021-02-19 01:17:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/telyo/androidstudiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.telyo.androidstudiotemplate.services
+
+import com.github.telyo.androidstudiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
